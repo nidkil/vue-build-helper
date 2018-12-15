ce2f1657c50615ac6925ae069931eca5588b3db2
nidkil <info@nidkil.com>
2018-12-15 21:58:12 +0100

    fix(bin entry): loading dist then src
    In prodcution the bin entry script should first try to load the dist version of the module, only if
    the dist does not exist fallback to the src version.

--- bin/vue-build-helper.js
+++ bin/vue-build-helper.js
@@ -3,7 +3,15 @@
 const updater = require('update-notifier')
 const pkg = require('../package.json')
 const { requireIfElse } = require('../src/common/module-helpers')
-const cli = requireIfElse('../dist/vue-build-helper.cjs', '../src/vue-build-helper-cli')
+
+let cli = null
+try {
+  // First try to load dist version
+  cli = require('../dist/vue-build-helper.cjs')
+} catch (e) {
+  // Fallback to src version
+  cli = require('../src/vue-build-helper-cli')
+}
 
 // Check if there is a new version of the module is available, if so show a warning message
 updater({ pkg }).notify()
