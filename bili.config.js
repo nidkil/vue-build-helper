3fbc5e15eba335e8586ce7bf474d97d149a57154
nidkil <info@nidkil.com>
2018-12-16 15:33:35 +0100

    build(bili): removed umd
    Removed umd version as the lib is not intended for browsers.

--- bili.config.js
+++ bili.config.js
@@ -1,17 +1,12 @@
-const builtins = require('rollup-plugin-node-builtins')
 module.exports = {
   input: './src/vue-build-helper-cli.js',
   outDir: './dist',
-  format: ['es', 'umd', 'umd-min', 'cjs'],
-  moduleName: 'vue-build-helper',
+  format: ['es', 'cjs'],
   name: 'vue-build-helper',
   filename: '[name][suffix].js',
   banner: true,
   // target: 'node',
   alias: {
     '@': require('path').resolve(__dirname, 'src')
-  },
-  plugin: [
-    builtins()
-  ]
+  }
 }
